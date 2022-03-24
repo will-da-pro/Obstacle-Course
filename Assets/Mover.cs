961dac61c0b9a2aa22c8363c648f251f2421875e
will-da-pro <56065795+will-da-pro@users.noreply.github.com>
2022-03-24 11:27:56 +1100

    Inertia

--- Assets/Mover.cs
+++ Assets/Mover.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Mover : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 10f;
+    float inetiaX = 0;
+    float inertiaY = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -28,6 +30,17 @@ public class Mover : MonoBehaviour
     {
         float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        if xValue >=0 
+        {
+            if inertiaX == 0 
+            {
+                inertiaX = 0.1
+            }
+            else
+            {
+                xValue = xValue * inertiaX;
+            }
+        }
         transform.Translate(xValue, 0, zValue);
     }
 }
